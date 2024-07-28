14b63fac057d0aa8b06f55bbb679b04887705e8c
Nguyen Thai Binh <binhnt.dev@icloud.com>
2024-07-28 21:59:42 +0800

    Add application initialisation flow

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -1,1 +1,17 @@
+use sqlx::SqlitePool;
+use thiserror::Error;
+
 mod auth;
+
+/// Errors occurring while initialising the application
+#[derive(Debug, Error)]
+pub enum AppError {
+    #[error("Unable to bind socket")]
+    SocketError(#[from] std::io::Error),
+}
+
+type Result<T> = std::result::Result<T, AppError>;
+
+pub async fn serve(db: SqlitePool) -> Result<()> {
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,31 @@
+use std::env;
+
+use anyhow::Context;
+use sqlx::SqlitePool;
+use tracing_subscriber::{filter, fmt, layer::SubscriberExt, util::SubscriberInitExt};
+
 mod http;
 
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    // load debug environment variables
+    // this allows for fast iteration in development across multiple machines
+    // in prod, all vars will be set prior to deployment
+    if cfg!(debug_assertions) {
+        dotenv::dotenv().ok();
+    }
+
+    // initialise tracing subscriber
+    tracing_subscriber::registry()
+        .with(fmt::layer())
+        .with(filter::EnvFilter::from_default_env())
+        .init();
+
+    // start database connection pool
+    let db_url = env::var("DATABASE_URL").context("DATABASE_URL is not set")?;
+    let db_pool = SqlitePool::connect(&db_url).await.context("The application cannot establish a database connection")?;
+    
+    // serve the application
+    http::serve(db_pool).await?;
+    Ok(())
 }
